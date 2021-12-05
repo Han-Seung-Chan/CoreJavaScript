031aa863e3eee2278444bb3762587514ab49f2ff
seungchanHan <seungchanhan00@gmail.com>
2021-12-05 23:59:37 +0900

    this 예제

---
+++ thisFromGlobal.js
@@ -0,0 +1,2 @@
+/* 전역공간에서 this를 사용하면 전역객체를 가르킨다 ex) web = window, node.js = global
+window 와 global 은 ECMAScript 에서 정의 한 객체가 아니라 ECMAScript가 '전역 객체는 이러이러해야 한다' 라고 정의 해둔 내용에 따라서 런타임 에사 제공하는 구현체 이다 */
